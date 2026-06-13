0778a3238338db30bb723add31b1242d6a3745b6
Donovan_DMC <hewwo@yiff.rocks>
2026-06-12 19:54:42 -0500

    Fix csv import

--- lib/e621/csv_importable.rb
+++ lib/e621/csv_importable.rb
@@ -7,8 +7,11 @@ module E621
 
     sig { params(csv_path: String).returns(T.untyped) }
     def import_from_csv(csv_path)
-      raw = T.unsafe(self).connection.raw_connection
-      raw.copy_data("COPY #{T.unsafe(self).quoted_table_name} FROM STDIN WITH (FORMAT CSV, HEADER TRUE)") do
+      model = T.unsafe(self)
+      headers = File.open(csv_path, "rb", &:readline).chomp
+      columns = headers.split(",").map { |h| model.connection.quote_column_name(h.strip) }.join(", ")
+      raw = model.connection.raw_connection
+      raw.copy_data("COPY #{model.quoted_table_name} (#{columns}) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)") do
         File.open(csv_path, "rb") do |f|
           raw.put_copy_data(f.read(65_536)) until f.eof?
         end
--- lib/e621_export_downloader/version.rb
+++ lib/e621_export_downloader/version.rb
@@ -4,7 +4,7 @@
 # loaded by bundler
 module E621ExportDownloader
   module Constants
-    VERSION = "0.0.10"
+    VERSION = "0.0.11"
     WEBSITE = "https://github.com/DonovanDMC/E621ExportDownloader.rb"
   end
 end
